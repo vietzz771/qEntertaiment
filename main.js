dc2d80ed07af5221ff77315eca98dca0e3c4d073
viet <vietzz771@gmail.com>
2022-05-27 21:47:43 +0700

    change color hover nav link, icon

--- main.js
+++ main.js
@@ -3,3 +3,10 @@ function classToggle() {
 	navs.forEach((nav) => nav.classList.toggle('nav__toggleShow'));
 }
 document.querySelector('.nav__toggle').addEventListener('click', classToggle);
+
+const navLink = document.querySelectorAll('.nav__items');
+function linkAction() {
+	// const navMenu = document.querySelectorAll('nav__items');
+	navLink.forEach((nav) => nav.classList.remove('nav__toggleShow'));
+}
+navLink.forEach((n) => n.addEventListener('click', linkAction));
