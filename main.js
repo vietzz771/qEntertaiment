23c674d6ee6cb8ff5f588dde8736d4b264c096f3
viet <vietzz771@gmail.com>
2022-05-27 19:27:19 +0700

    add nav toggle

--- main.js
+++ main.js
@@ -1,8 +1,5 @@
 function classToggle() {
 	const navs = document.querySelectorAll('.nav__items');
-	console.log(navs);
 	navs.forEach((nav) => nav.classList.toggle('nav__toggleShow'));
 }
-
 document.querySelector('.nav__toggle').addEventListener('click', classToggle);
-console.log(document.querySelector('.nav__menu'));
