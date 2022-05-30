060a3a52d706357d34a792bc355abc990169c0f5
viet <vietzz771@gmail.com>
2022-05-30 18:28:16 +0700

    add service page

--- main.js
+++ main.js
@@ -1,9 +1,11 @@
+// Display nav toggle
 function classToggle() {
 	const navs = document.querySelectorAll('.nav__items');
 	navs.forEach((nav) => nav.classList.toggle('nav__toggleShow'));
 }
 document.querySelector('.nav__toggle').addEventListener('click', classToggle);
 
+// Remove nav menu when click nav items
 const navLink = document.querySelectorAll('.nav__items');
 function linkAction() {
 	// const navMenu = document.querySelectorAll('nav__items');
